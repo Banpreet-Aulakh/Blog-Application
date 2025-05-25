15cb98208d660f89106c8d82f466a3f177438c2f
Banpreet Aulakh <banpreetaulakh@gmail.com>
2025-05-24 17:33:33 -0700

    Initial ejs layout

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("index");
 });
 
 app.listen(port, () => {
